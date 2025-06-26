68ebf3159c387490adb6cc6ff467b1b8ae6e4bc4
maxpo-dev <dev.maxpo@gmail.com>
2025-06-26 10:58:09 +0530

    cms updated

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -1,18 +1,21 @@
+import { ProjectsMetrics } from "@/components/landingPage/projectsMetrics";
+import StatisticsChart from "@/components/landingPage/statisticsChart";
 import type { Metadata } from "next";
 import React from "react";
 
-
 export const metadata: Metadata = {
-  title:
-    "Next.js E-commerce Projects | Maxpo CMS - Next.js Projects Template",
-  description: "This is Next.js Home for Maxpo CMS Projects Template",
+  title: "Maxpo CMS ",
+  description: "",
 };
 
 export default function Projects() {
   return (
     <div className="grid grid-cols-12 gap-4 md:gap-6">
-      <div className="col-span-12 space-y-6 xl:col-span-7">
-
+      <div className="col-span-12 space-y-6">
+        <ProjectsMetrics />
+      </div>
+        <div className="col-span-12">
+        <StatisticsChart />
       </div>
     </div>
   );
---
+++ src/components/landingPage/projectsMetrics.tsx
@@ -0,0 +1,50 @@
+"use client";
+import React from "react";
+import { BoxIconLine, GroupIcon } from "@/icons";
+
+const data = [
+  {
+    name: "Future Prop Tech",
+    description: "The Tech Show",
+    icon: GroupIcon,
+    selected: true,
+  },
+  {
+    name: "RevolutionEv",
+    description: "The Tech Show",
+    icon: BoxIconLine,
+  },
+];
+
+export const ProjectsMetrics = () => {
+  return (
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6">
+      {data.map((item, index) => (
+        <div
+          className={
+            "rounded-2xl border  bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6" +
+            (item.selected ? " border-brand-500" : "border-gray-200")
+          }
+          key={index}
+        >
+          <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+            {item.icon && (
+              <item.icon className="text-gray-800 size-6 dark:text-white/90" />
+            )}
+          </div>
+
+          <div className="flex items-end justify-between mt-5">
+            <div>
+              <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
+                {item.name}
+              </h4>
+              <span className="text-sm text-gray-500 dark:text-gray-400">
+                {item.description}
+              </span>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
